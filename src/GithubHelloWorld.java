3e6d3e491d8af98afef89083467f0a0635395803
Gabriel Walters <gwalters@lhschools.org>
2020-01-26 21:09:18 -0500

    intellij commit

---
+++ src/GithubHelloWorld.java
@@ -0,0 +1,14 @@
+public class GithubHelloWorld {
+    public static void main(String[] args) {
+
+        
+    }
+
+    public static void helloWorld(){
+        System.out.println("Hello World");
+    }
+
+
+
+
+}
