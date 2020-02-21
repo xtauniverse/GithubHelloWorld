0f68c69a6b0f91c2d3dc26c44cd9e6892b9445c5
Xavier O <xavier.odonovan.2021@student.lhschools.org>
2020-02-21 14:05:03 -0500

    called Hello World Method

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -1,11 +1,17 @@
 public class GithubHelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
+        System.out.println("Hello Github World");
+        helloWorld();
     }
 
     public static void helloWorld(){
+
         System.out.println("Hello World");
+
+        System.out.println("Hello Github World");
+
+
+
     }
 
 
